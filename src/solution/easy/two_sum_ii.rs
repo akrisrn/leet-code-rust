e8d1d0aa50cda1e7315cf890f1a22537f01caa52
AkrISrn <shairkarine@gmail.com>
2019-10-30 12:50:52 +0800

    complete two_sum_ii

--- src/solution/easy/two_sum_ii.rs
+++ src/solution/easy/two_sum_ii.rs
@@ -1,4 +1,5 @@
 use crate::Solution;
+use std::collections::HashMap;
 
 /// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 ///
@@ -18,5 +19,20 @@ use crate::Solution;
 /// > Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
 ///
 impl Solution {
-    pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {}
+    pub fn two_sum_ii(numbers: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map = HashMap::new();
+        for (i, number) in numbers.iter().enumerate() {
+            if map.contains_key(number) {
+                return vec![map[number] as i32, (i + 1) as i32];
+            } else {
+                map.insert(target - *number, i + 1);
+            }
+        }
+        vec![]
+    }
+}
+
+#[test]
+fn test_a() {
+    assert_eq!(Solution::two_sum_ii(vec![2, 7, 11, 15], 9), vec![1, 2]);
 }
