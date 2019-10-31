ca36c5e911b60bcde9172d19002fea9f8095ed39
AkrISrn <shairkarine@gmail.com>
2019-10-31 16:26:22 +0800

    add src/solution/easy/longest_common_prefix.rs

--- src/solution/easy.rs
+++ src/solution/easy.rs
@@ -4,3 +4,4 @@ mod two_sum_iv;
 mod reverse_integer;
 mod palindrome_number;
 mod roman_to_integer;
+mod longest_common_prefix;
---
+++ src/solution/easy/longest_common_prefix.rs
@@ -0,0 +1,23 @@
+use crate::Solution;
+
+/// https://leetcode.com/problems/longest-common-prefix/
+///
+/// Write a function to find the longest common prefix string amongst an array of strings.
+/// If there is no common prefix, return an empty string `""`.
+///
+/// # Note
+///
+/// All given inputs are in lowercase letters `a-z`.
+///
+/// # Example
+///
+/// > Input: ["flower", "flow", "flight"]
+/// > Output: "fl"
+///
+/// > Input: ["dog", "racecar", "car"]
+/// > Output: ""
+/// > Explanation: There is no common prefix among the input strings.
+///
+impl Solution {
+    pub fn longest_common_prefix(strs: Vec<String>) -> String {}
+}
