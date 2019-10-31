0ef18babd79c04e846622cd35f86908d3dcfa60f
AkrISrn <shairkarine@gmail.com>
2019-10-31 17:29:18 +0800

    complete longest_common_prefix

--- src/solution/easy/longest_common_prefix.rs
+++ src/solution/easy/longest_common_prefix.rs
@@ -19,5 +19,32 @@ use crate::Solution;
 /// > Explanation: There is no common prefix among the input strings.
 ///
 impl Solution {
-    pub fn longest_common_prefix(strs: Vec<String>) -> String {}
+    pub fn longest_common_prefix(strs: Vec<String>) -> String {
+        let mut prefix_chars: Vec<u8> = strs[0].bytes().collect();
+        for str in &strs[1..] {
+            for (i, ch) in str.bytes().enumerate() {
+                if i < prefix_chars.len() {
+                    if prefix_chars[i] != ch {
+                        prefix_chars = prefix_chars[..i].to_owned();
+                        if prefix_chars.len() == 0 {
+                            return "".to_string();
+                        }
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+        String::from_utf8(prefix_chars).unwrap()
+    }
+}
+
+#[test]
+fn test_a() {
+    assert_eq!(Solution::longest_common_prefix(vec!["flower".to_string(), "flow".to_string(), "flight".to_string()]), "fl");
+}
+
+#[test]
+fn test_b() {
+    assert_eq!(Solution::longest_common_prefix(vec!["dog".to_string(), "racecar".to_string(), "car".to_string()]), "");
 }
