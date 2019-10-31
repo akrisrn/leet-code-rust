97440f23ce27aefa5ba547d652d30221e39bf68b
AkrISrn <shairkarine@gmail.com>
2019-10-31 11:39:02 +0800

    add is_palindrome_0ms

--- src/solution/easy/palindrome_number.rs
+++ src/solution/easy/palindrome_number.rs
@@ -29,6 +29,21 @@ impl Solution {
         bytes.reverse();
         String::from_utf8(bytes).unwrap() == x.to_string()
     }
+
+    pub fn is_palindrome_0ms(x: i32) -> bool {
+        if x < 0 {
+            return false;
+        }
+        let digits = (x as f64).log10() as u32 + 1;
+        for i in 0..(digits / 2) {
+            let left = x / (10 as i32).pow(digits - i - 1) % 10;
+            let right = x / (10 as i32).pow(i) % 10;
+            if left != right {
+                return false;
+            }
+        }
+        true
+    }
 }
 
 #[test]
