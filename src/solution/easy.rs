94dc1a22b1883ebf4306f268a24e8f6840598697
AkrISrn <shairkarine@gmail.com>
2019-10-31 10:55:59 +0800

    add src/solution/easy/palindrome_number.rs

--- src/solution/easy.rs
+++ src/solution/easy.rs
@@ -2,3 +2,4 @@ mod two_sum;
 mod two_sum_ii;
 mod two_sum_iv;
 mod reverse_integer;
+mod palindrome_number;
---
+++ src/solution/easy/palindrome_number.rs
@@ -0,0 +1,28 @@
+use crate::Solution;
+
+/// https://leetcode.com/problems/palindrome-number/
+///
+/// Determine whether an integer is a palindrome.
+/// An integer is a palindrome when it reads the same backward as forward.
+///
+/// # Follow up
+///
+/// Could you solve it without converting the integer to a string?
+///
+/// # Example
+///
+/// > Input: 121
+/// > Output: true
+///
+/// > Input: -121
+/// > Output: false
+/// > Explanation: From left to right, it reads -121. From right to left, it becomes 121-.
+/// > Therefore it is not a palindrome.
+///
+/// > Input: 10
+/// > Output: false
+/// > Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+///
+impl Solution {
+    pub fn is_palindrome(x: i32) -> bool {}
+}
