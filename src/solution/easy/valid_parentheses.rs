43f6245890389a056d5de3f2057de35cb4625219
AkrISrn <shairkarine@gmail.com>
2019-11-01 09:55:16 +0800

    complete is_valid

--- src/solution/easy/valid_parentheses.rs
+++ src/solution/easy/valid_parentheses.rs
@@ -1,4 +1,5 @@
 use crate::Solution;
+use std::collections::HashMap;
 
 /// https://leetcode.com/problems/valid-parentheses/
 ///
@@ -30,5 +31,60 @@ use crate::Solution;
 /// > Output: true
 ///
 impl Solution {
-    pub fn is_valid(s: String) -> bool {}
+    pub fn is_valid(s: String) -> bool {
+        let map: HashMap<char, char> = vec![
+            ('(', ')'),
+            ('{', '}'),
+            ('[', ']'),
+        ].into_iter().collect();
+        let mut stack = vec![];
+        for char in s.chars() {
+            match char {
+                ')' | '}' | ']' => {
+                    if let Some(ch) = stack.pop() {
+                        if let Some(&c) = map.get(&ch) {
+                            if c == char {
+                                break;
+                            }
+                        }
+                    }
+                    return false;
+                }
+                _ => {
+                    stack.push(char)
+                }
+            }
+        }
+        true
+    }
+}
+
+#[test]
+fn test_a() {
+    assert!(Solution::is_valid("()".to_string()));
+}
+
+#[test]
+fn test_b() {
+    assert!(Solution::is_valid("()[]{}".to_string()));
+}
+
+#[test]
+fn test_c() {
+    assert!(!Solution::is_valid("(]".to_string()));
+}
+
+#[test]
+fn test_d() {
+    assert!(!Solution::is_valid("([)]".to_string()));
+}
+
+#[test]
+fn test_e() {
+    assert!(Solution::is_valid("{[]}".to_string()));
+}
+
+#[test]
+fn test_f() {
+    assert!(Solution::is_valid("".to_string()));
 }
