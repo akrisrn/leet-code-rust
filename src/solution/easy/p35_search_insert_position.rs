26e6fa0a4d238b0dc67d885f92d9d8e9dd563b07
AkrISrn <shairkarine@gmail.com>
2019-11-04 17:14:03 +0800

    complete search_insert

--- src/solution/easy/p35_search_insert_position.rs
+++ src/solution/easy/p35_search_insert_position.rs
@@ -21,5 +21,37 @@ use crate::Solution;
 /// > Output: 0
 ///
 impl Solution {
-    pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {}
+    pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+        let mut index = 0;
+        for (i, &num) in nums.iter().enumerate() {
+            if num == target {
+                return i as i32;
+            } else if num < target {
+                index = i as i32 + 1
+            } else if num > target {
+                return index;
+            }
+        }
+        index
+    }
+}
+
+#[test]
+fn test_a() {
+    assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 5), 2)
+}
+
+#[test]
+fn test_b() {
+    assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 2), 1)
+}
+
+#[test]
+fn test_c() {
+    assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 7), 4)
+}
+
+#[test]
+fn test_d() {
+    assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 0), 0)
 }
