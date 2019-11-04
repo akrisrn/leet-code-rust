63b11366242541a545d52e16cad7dc0d32e1066a
AkrISrn <shairkarine@gmail.com>
2019-11-04 11:42:51 +0800

    complete remove_duplicates

--- src/solution/easy/remove_duplicates_from_sorted_array.rs
+++ src/solution/easy/remove_duplicates_from_sorted_array.rs
@@ -35,5 +35,33 @@ use crate::Solution;
 /// > It doesn't matter what values are set beyond the returned length.
 ///
 impl Solution {
-    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {}
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        let mut some_pre = None;
+        let mut i = 0;
+        while i < nums.len() {
+            if let Some(pre) = some_pre {
+                if pre == nums[i] {
+                    nums.remove(i);
+                    i -= 1;
+                }
+            }
+            some_pre = Some(nums[i]);
+            i += 1;
+        }
+        nums.len() as i32
+    }
+}
+
+#[test]
+fn test_a() {
+    let mut nums = vec![1, 1, 2];
+    assert_eq!(Solution::remove_duplicates(&mut nums), 2);
+    assert_eq!(nums, vec![1, 2])
+}
+
+#[test]
+fn test_b() {
+    let mut nums = vec![0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+    assert_eq!(Solution::remove_duplicates(&mut nums), 5);
+    assert_eq!(nums, vec![0, 1, 2, 3, 4])
 }
