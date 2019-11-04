1d4043117d9c0492ae10f68a00f76f0b2be769d5
AkrISrn <shairkarine@gmail.com>
2019-11-04 17:21:30 +0800

    small fix

--- src/solution/easy/p38_count_and_say.rs
+++ src/solution/easy/p38_count_and_say.rs
@@ -12,7 +12,7 @@ use crate::Solution;
 ///
 /// `1` is read off as `"one 1"` or `11`.
 /// `11` is read off as `"two 1s"` or `21`.
-/// `21` is read off as `"one 2, then one 1"` or `1211`.
+/// `21` is read off as `"one 2`, then `one 1"` or `1211`.
 ///
 /// Given an integer n where 1 ≤ n ≤ 30, generate the n^th term of the count-and-say sequence.
 /// Note: Each term of the sequence of integers will be represented as a string.
