af9fddd3731b11be055f92667de3813e1db46119
AkrISrn <shairkarine@gmail.com>
2019-11-06 15:32:53 +0800

    complete max_sub_array

--- src/solution/easy/p53_maximum_subarray.rs
+++ src/solution/easy/p53_maximum_subarray.rs
@@ -17,5 +17,24 @@ use crate::Solution;
 /// > Explanation: [4, -1, 2, 1] has the largest sum = 6.
 ///
 impl Solution {
-    pub fn max_sub_array(nums: Vec<i32>) -> i32 {}
+    pub fn max_sub_array(nums: Vec<i32>) -> i32 {
+        let mut max = 0;
+        let mut cur = 0;
+        for num in nums {
+            if cur + num <= 0 {
+                cur = 0;
+            } else {
+                cur += num;
+                if cur > max {
+                    max = cur;
+                }
+            }
+        }
+        max
+    }
+}
+
+#[test]
+fn test_a() {
+    assert_eq!(Solution::max_sub_array(vec![-2, 1, -3, 4, -1, 2, 1, -5, 4]), 6)
 }
