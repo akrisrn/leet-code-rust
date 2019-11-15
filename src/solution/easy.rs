9d49a9f096ee5a71ee13c6a1f40d279d6b290729
AkrISrn <shairkarine@gmail.com>
2019-11-15 14:34:09 +0800

    add src/solution/easy/p69_sqrtx.rs

--- src/solution/easy.rs
+++ src/solution/easy.rs
@@ -22,3 +22,4 @@ mod p104_maximum_depth_of_binary_tree;
 mod p121_best_time_to_buy_and_sell_stock;
 mod p136_single_number;
 mod p169_majority_element;
+mod p69_sqrtx;
---
+++ src/solution/easy/p69_sqrtx.rs
@@ -0,0 +1,32 @@
+use crate::Solution;
+
+/// https://leetcode.com/problems/sqrtx/
+///
+/// Implement `int sqrt(int x)`.
+/// Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
+/// Since the return type is an integer,
+/// the decimal digits are truncated and only the integer part of the result is returned.
+///
+/// # Example
+///
+/// > Input: 4
+/// > Output: 2
+///
+/// > Input: 8
+/// > Output: 2
+/// > Explanation: The square root of 8 is 2.82842..., and since
+/// >              the decimal part is truncated, 2 is returned.
+///
+impl Solution {
+    pub fn my_sqrt(x: i32) -> i32 {}
+}
+
+#[test]
+fn test_a() {
+    assert_eq!(Solution::my_sqrt(4), 2)
+}
+
+#[test]
+fn test_b() {
+    assert_eq!(Solution::my_sqrt(8), 2)
+}
